4a1e3aeda31dd76e73a2acd0d73b95ef560f86d8
doukod <76803369+doukod@users.noreply.github.com>
2020-12-31 02:47:01 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-west-1"
+  access_key = "AKIAJATTFOOSWG4Y2YEA"
+  secret_key = "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"
+}
+
 terraform {
   backend "s3" {
     bucket = "kplabs-remote-backend"
