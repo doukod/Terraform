4bea8bf6a904276e75cc30a60eea1a11cf29bbd7
doukod <76803369+doukod@users.noreply.github.com>
2020-12-31 02:52:18 -0500

    Create tf-import.tf

---
+++ tf-import.tf
@@ -0,0 +1,20 @@
+ec2.tf
+resource "aws_instance" "myec2" {
+  ami = "ami-bf5540df"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-6ae7d613", "sg-53370035"]
+  key_name = "remotepractical"
+  subnet_id = "subnet-9e3cfbc5"
+
+  tags {
+    Name = "manual"
+  }
+}
+providers.tf
+provider "aws" {
+  region = "us-west-1"
+}
+
+
+#Command To Import Resource
+terraform import aws_instance.myec2 i-041886ebb7e9bd20
