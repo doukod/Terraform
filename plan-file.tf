afb813a84399f486d4f6a575d89814b7715c3538
doukod <76803369+doukod@users.noreply.github.com>
2021-01-04 01:08:02 -0500

    Update plan-file.tf

--- plan-file.tf
+++ plan-file.tf
@@ -5,12 +5,12 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
+resource "aws_instance" "desired instance name" {
+   ami = ""
    instance_type = "t2.micro"
 }
 
 
 Commands Used:
-terraform plan -out=demopath
-terraform apply demopath
+terraform plan -out=file name
+terraform apply file name
