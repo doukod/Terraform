c03d79d055202917b43f504f42ae41f8a67fd6b4
doukod <76803369+doukod@users.noreply.github.com>
2021-01-04 00:42:35 -0500

    Update Provider.versioning.tf

--- Provider.versioning.tf
+++ Provider.versioning.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region     = "us-west-2"
+  region     = "us-east-1"
   access_key = "YOUR-ACCESS-KEY"
   secret_key = "YOUR-SECRET-KEY"
   version    = ">=2.10,<=2.30"
 }
 
-resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
+resource "aws_instance" "instance name" {
+   ami = ""
    instance_type = "t2.micro"
 }
