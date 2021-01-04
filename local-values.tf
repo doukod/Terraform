10ad1c5a19430205e3a03819083fd63d58e008c0
doukod <76803369+doukod@users.noreply.github.com>
2021-01-04 01:05:09 -0500

    Update local-values.tf

--- local-values.tf
+++ local-values.tf
@@ -6,24 +6,24 @@ provider "aws" {
 
 locals {
   common_tags = {
-    Owner = "DevOps Team"
+    Owner = "Linux Team"
     service = "backend"
   }
 }
-resource "aws_instance" "app-dev" {
-   ami = "ami-082b5a644766e0e6f"
+resource "aws_instance" "desired instance name" {
+   ami = ""
    instance_type = "t2.micro"
    tags = local.common_tags
 }
 
 resource "aws_instance" "db-dev" {
-   ami = "ami-082b5a644766e0e6f"
+   ami = ""
    instance_type = "t2.small"
    tags = local.common_tags
 }
 
 resource "aws_ebs_volume" "db_ebs" {
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
   size              = 8
   tags = local.common_tags
 }
