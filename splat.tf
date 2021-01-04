cb6c834d94ebd968838812bd6632064a43046fe0
doukod <76803369+doukod@users.noreply.github.com>
2021-01-04 01:16:36 -0500

    Update splat.tf

--- splat.tf
+++ splat.tf
@@ -3,12 +3,12 @@ provider "aws" {
   access_key = "YOUR-ACCESS-KEY"
   secret_key = "YOUR-SECRET-KEY"
 }
-resource "aws_iam_user" "lb" {
+resource "aws_iam_user" "desired name" {
   name = "iamuser.${count.index}"
   count = 2
   path = "/system/"
 }
 
 output "arns" {
-  value = aws_iam_user.lb[*].arn
+  value = aws_iam_user.desired name[*].arn
 }
