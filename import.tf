efc2a273f1482f2ef426f144b2bacd37d2ac386b
doukod <76803369+doukod@users.noreply.github.com>
2021-01-04 00:38:10 -0500

    Update and rename tf-import.tf to import.tf

---
+++ import.tf
@@ -0,0 +1,22 @@
+#ec2.tf
+resource "aws_instance" "myec2" {
+  ami = " "
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg........", "sg.........."]
+  key_name = "keypair name"
+  subnet_id = "subnet........."
+
+  tags {
+    Name = "desired name"
+  }
+}
+
+
+#providers.tf
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+#Command To Import Resource
+terraform import aws_instance.myec2 instanceID
